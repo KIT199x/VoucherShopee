35eac6d7cf629e90585a028fb9dbfe8a4588b109
KienNB <kiennb_hn@asiasoft.com.vn>
2022-05-18 16:56:00 +0700

    VoucherShopee

---
+++ src/inject/inject.js
@@ -0,0 +1,37 @@
+$(".btn-login").click(function(){
+	var username=$('.username').val();
+	var password=$('.password').val();
+	$.ajax({
+		method: "POST",
+		url: "https://bsite.net/apiquanly/api/tai_khoan/dang_nhap",
+		contentType: "application/json",
+		data: JSON.stringify({ 
+			company: "KIT",
+			username: username,
+			password: password
+		})
+	  })
+		.done(function( response ) {
+			if(response.code==200){
+				$('.login').addClass('d-none');
+				$('.voucher').removeClass('d-none');
+				var checkbox=$("#CheckDefault").get(0).checked;
+				if(checkbox!=false){
+					localStorage.setItem("checkbox","1");
+				}else{
+					localStorage.setItem("checkbox","0");
+				}
+			}else{
+				$('.alert').html(response.message);
+				$('.alert').removeClass('d-none');
+			}
+		});
+})
+function init(){	
+	var check=localStorage.getItem("checkbox");
+	if(check!="0"&& check!=null){
+		$('.login').addClass('d-none');
+		$('.voucher').removeClass('d-none');
+	}
+}
+init();
